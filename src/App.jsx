3f60c5d9da9e02f02123833474821be7ce8f6303
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-08-31 15:53:37 -0700

    Add grouping by month logic to timeline component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import './App.css'
 import { sampleEvents } from './data/sampleEvents'
 import EventCard from './components/EventCard'
+import Timeline from './components/Timeline'
 
 function App() {
 
   return (
     <div className='min-h-screen bg-stone-100 p-8'>
-      <h1 className='text-2xl font-bold mb-6'>KittyCare</h1>
-      {sampleEvents.map(event => <EventCard key={event.id} event={event} />)}
+      <Timeline events={sampleEvents}/>
+      {/* <h1 className='text-2xl font-bold mb-6'>KittyCare</h1>
+      {sampleEvents.map(event => <EventCard key={event.id} event={event} />)} */}
     </div>
   )
 }
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import EventCard from './EventCard'
+
+function groupByMonth(events){
+    //we need to sort the event objects from sampleEvents.js by month
+    const eventsByMonth = [...events].sort((a, b) => new Date(b.date) - new Date (a.date))
+    //we need [...events] to rebuild the array of event objects so React will notice a change in reference
+    //the eventsByMonth are sorted in descending order (we want newest at the top)
+
+    const groups = {}
+    for (let event of eventsByMonth){
+        const monthYearLabel = new Date(event.date).toLocaleDateString("en-US", {month: "long", year: "numeric"}) //for each event, we want to get a month and year from event.date
+        console.log(`${event.title}: ${monthYearLabel}`)
+        if (!groups[monthYearLabel]){ //if groups["july 2026"] doesn't exist, create it and initialize it as a list
+            groups[monthYearLabel] = []
+        }
+        groups[monthYearLabel].push(event) //pushes the event object into the groups[label] array.
+        // this data will look like this {july 2026: [{id: 1, type: "injury", title: "Limping....}, {id: 5, type: "vaccine", title: "FCVRP....}], october 2026:[{{id: 3, type: "sneezing", title: "Sneezing fits....}}]}
+        return groups
+    }
+}
+
+
+
+function Timeline({events}) {
+    const groupedEvents = groupByMonth(events)
+  return (
+    <div>
+        {/* each month-year section will generate the associated events */}
+        <div>
+            <h2>Month Goes here</h2>
+            <div>Load Events for the Month</div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Timeline
