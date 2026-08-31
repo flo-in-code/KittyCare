58e2ab4ed431347bba5b04a7c7c8a1c60f7c16bf
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-08-30 23:22:35 -0700

    Update date and time formatting on event card

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,9 +1,16 @@
 // import React from 'react'
 
 function EventCard({ event }) {
+  const eventDate = new Date(event.date) //turn event.date (ISO string) into a date object
+
   return (
     <div className="bg-white rounded-xl border border-gray-200 p-4">
-      <p className="text-stone-500">{event.date}</p>
+      <p className="text-stone-500">
+        {eventDate.toLocaleDateString("en-US", {month: "short", day: "numeric"})} 
+        {" - "}
+        {eventDate.toLocaleTimeString("en-US", {hour: "numeric", minute: "2-digit"})}
+        {/* processes the date object into formatted date and time */}
+      </p>
       <div className="flex justify-between items-end">
         <h3 className="font-semibold text-stone-800">{event.title}</h3>
         <span className="text-xs">{event.type}</span>
