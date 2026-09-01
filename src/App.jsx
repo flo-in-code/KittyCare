24d4430ac97e8bbf84d2320cec4d5726e17391b8
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-08-31 23:30:05 -0700

    Add basic styling to sidebar and timeline

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import './App.css'
 import { sampleEvents } from './data/sampleEvents'
 import EventCard from './components/EventCard'
 import Timeline from './components/Timeline'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <div className='min-h-screen bg-stone-100 p-8'>
-      <h1 className='text-2xl font-bold mb-6'>KittyCare</h1>
-      <Timeline events={sampleEvents}/>
+    <div className=' flex min-h-screen bg-stone-100 pr-8 space-x-4'>
+      <Sidebar />
+      <div className='flex-1 py-8'>
+        <h1 className='text-2xl font-bold mb-6'>Timeline of Events</h1>
+        <Timeline events={sampleEvents} />
+      </div>
     </div>
   )
 }
