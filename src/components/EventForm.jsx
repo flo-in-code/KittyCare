fa4d0d7c165b48f13988b021a80bee2287e163ff
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-09-01 23:27:48 -0700

    Update styling, dropdown values, and add buttons to event form

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -1,50 +1,55 @@
 
 function EventForm() {
     return (
-        <form className="flex flex-col py-4 px-4 bg-white rounded-2xl m-8 space-y-2">
+        <form className="flex flex-col py-6 px-8 bg-white rounded-2xl m-8 space-y-2">
             <h1 className="text-xl font-bold ">{`Log Health Event`}</h1>
             <h4>{`for CAT NAME`}</h4>
             <div className="border-t border-stone-300"></div>
             {/* Category Selection ------------------------------- */}
-            <p className="text-sm font-semibold text-stone-700">What happened?</p>
+            <p className="text-sm font-semibold text-stone-700 mt-2">What happened?</p>
             <select className="border border-stone-300 bg-taupe-100 pl-2 py-1 rounded-md">
-                <option value="" disabled selected>Selection an option</option>
-                <option value="">Behavior</option>
-                <option value="">Diet</option>
-                <option value="">Coughing</option>
-                <option value="">Injury</option>
-                <option value="">Medication</option> 
-                <option value="">Stool</option>
-                <option value="">Surgery</option>
-                <option value="">Vet Visit</option>
-                <option value="">Vomiting</option>
-                <option value="">Weight</option>
-                <option value="">Other</option>
+                <option value="" disabled selected>Select an option</option>
+                <option value="behavior">Behavior</option>
+                <option value="diet">Diet</option>
+                <option value="coughing">Coughing</option>
+                <option value="injury">Injury</option>
+                <option value="medication">Medication</option>
+                <option value="stool">Stool</option>
+                <option value="surgery">Surgery</option>
+                <option value="vetVisit">Vet Visit</option>
+                <option value="vomiting">Vomiting</option>
+                <option value="weight">Weight</option>
+                <option value="other">Other</option>
             </select>
             {/* Date & Time -------------------------------- */}
-            <label htmlFor="" className="text-sm font-semibold text-stone-700">Date & Time</label>
+            <label htmlFor="" className="text-sm font-semibold text-stone-700 mt-2">Date & Time</label>
             <input type="datetime-local" className="border border-stone-300 bg-taupe-100 rounded-md pl-2 py-1"></input>
             {/* Title ---------------------------------------*/}
-            <label htmlFor="" className="text-sm font-semibold text-stone-700">Title</label>
-            <input type="text" className="border border-stone-300 bg-taupe-100 rounded-md pl-2 py-1" ></input>
+            <label htmlFor="" className="text-sm font-semibold text-stone-700 mt-2">Title</label>
+            <input type="text" placeholder="e.g. Vomiting 3x in one morning" className="border border-stone-300 bg-taupe-100 rounded-md pl-2 py-1" ></input>
             {/* Notes & Details ---------------------------------------*/}
-            <label className="text-sm font-semibold text-stone-700">Notes & Details</label>
-            <textarea name="" id="" className="border border-stone-300 bg-taupe-100 rounded-md pl-2 py-1"></textarea>
+            <label className="text-sm font-semibold text-stone-700 mt-2">Notes & Details</label>
+            <textarea name="" id="" placeholder="When did it start? How often? Any other symptoms? What did you observe?"
+                className="h-32 border border-stone-300 bg-taupe-100 rounded-md pl-2 py-1"></textarea>
             {/* "Severity tag" ---------------------------------------*/}
-            <label htmlFor="" className="text-sm font-semibold text-stone-700">Severity</label>
+            <label htmlFor="" className="text-sm font-semibold text-stone-700 mt-2">Severity</label>
             <div className="flex gap-2">
                 <button className="border border-stone-300 rounded-md px-4 py-2 text-stone-700">Mild</button>
                 <button className="border border-stone-300 rounded-md px-4 py-2 text-stone-700">Moderate</button>
                 <button className="border border-stone-300 rounded-md px-4 py-2 text-stone-700">Severe</button>
             </div>
-             {/* Vet Prep Flag ---------------------------------------*/}
-            <div className="flex gap-1 py-2">
+            {/* Vet Prep Flag ---------------------------------------*/}
+            <div className="flex gap-1 py-2 mt-2">
                 <input type="checkbox"></input>
                 <p className="text">Flag for vet appointment prep</p>
             </div>
-             {/* Photo & Video Upload ---------------------------------------*/}
+            {/* Photo & Video Upload ---------------------------------------*/}
             <div>
-                <p className="text-sm font-semibold text-stone-700 py-2">Upload photos or video</p>
+                <p className="text-sm font-semibold text-stone-700 py-2 mt-2">Upload photos or video</p>
+            </div>
+            <div className="flex gap-2">
+                <button className="flex-1 border border-stone-300 bg-stone-50 text-stone-700 rounded-md py-1">Cancel</button>
+                <button type="submit" className="flex-1 border border-stone-300 bg-emerald-600 text-white rounded-md py-1">Save Event</button>
             </div>
 
         </form>
