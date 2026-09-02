2820fb35173b8fd3fbe066dc53f1495c8c1a6107
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-09-01 22:36:15 -0700

    Fix svg stroke warnings

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,9 +17,9 @@ function Sidebar() {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="#607d8b"
-                        stroke-width="1"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="1"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                     >
                         <path d="M20 3v10a8 8 0 1 1 -16 0v-10l3.432 3.432a7.963 7.963 0 0 1 4.568 -1.432c1.769 0 3.403 .574 4.728 1.546l3.272 -3.546z" />
                         <path d="M2 16h5l-4 4" />
