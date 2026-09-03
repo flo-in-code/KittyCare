b3d8d6e3215ee5ac56a93ec2027d3b80de829bad
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-09-02 19:35:24 -0700

    Update sample cat data

--- src/data/sampleCats.js
+++ src/data/sampleCats.js
@@ -3,15 +3,23 @@ export const sampleCatProfiles = [
         id: 1,
         name: "Mochi",
         breed: "British Shorthair",
+        color: 'cream',
+        birthday: '2022-03-14',
+        weight: 9.2,
+        vetName: 'Dr.Chen',
+        vetPhone: '(415) 555-0100',
+        microchipID: '985141003456789'
     },
     {
         id: 2,
-        name: "Oliver",
-        breed: "Scottish Fold"
-    },
-    {
-        id: 3,
-        name: "Kyo",
-        breed: "Scottish Fold"
+        name: "Duchess",
+        breed: "Siamese",
+        color: 'Seal Point',
+        birthday: '2021-07-02',
+        weight: 8.1,
+        vetName: '',
+        vetPhone: '',
+        microchipID: ''
     }
+
 ]
