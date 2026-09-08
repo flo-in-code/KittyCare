782e239ef2bfdc6b71e7617ce958922356a4839d
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-09-07 19:08:01 -0700

    Rewrite GET POST DELETE routes for /cats and /events

--- server/index.js
+++ server/index.js
@@ -58,63 +58,119 @@ app.get('/', (req, res) => {
 //req - everything about the incoming request
 //res - tool for sending something back  res.send('...') sends back plain text and res.json({...}) sends data structures 
 
+//cat routes - expressJS
+// app.get('/cats', (req, res) => {
+//     res.json(cats)
+// })
+// // the route 'localhost:3001/cats' gives us the cat data
 
-app.get('/cats', (req, res) => {
+// app.post('/cats', (req, res) => {
+//     const newCat = req.body
+
+//     if(!newCat || !newCat.name){
+//         return res.status(400).json({error: 'missing required cat field'})
+//     }
+
+//     cats.push(newCat)
+//     res.status(201).json(newCat)
+// })
+
+// app.delete('/cats/:id', (req, res) =>{
+//     const {id} = req.params
+//     const index = cats.findIndex((cat) => String(cat.id) === id)
+    
+//     if (index === -1){
+//         return res.status(404).json({error: 'Cat not found'})
+//     }
+//     cats.splice(index, 1)
+//     return res.status(204).end()
+// })
+
+//cat routes - Mongoose
+app.get('/cats', async(req, res) => { //because node server need to talk to Mongoose, all routes will become async
+    const cats = await Cat.find() //queries database to get everything in the collection,  'cats'
     res.json(cats)
 })
-// the route 'localhost:3001/cats' gives us the cat data
 
-app.post('/cats', (req, res) => {
+app.post('/cats', async(req, res) => {
     const newCat = req.body
-
+    
     if(!newCat || !newCat.name){
-        return res.status(400).json({error: 'missing required cat field'})
+        return res.status(400).json({error: 'missing required field'})
     }
 
-    cats.push(newCat)
-    res.status(201).json(newCat)
+    const createdCat = await Cat.create(newCat) //creates new cat using the Cat model and mongoDB assigns it an _id. (replaces cats.push(newCat)) 
+    res.status(201).json(createdCat) //return is not necessary here because sending the response is the last line
 })
 
-app.delete('/cats/:id', (req, res) =>{
+app.delete('/cats/:id', async (req, res) =>{
     const {id} = req.params
-    const index = cats.findIndex((cat) => String(cat.id) === id)
-    
-    if (index === -1){
+    const deletedCat = await Cat.findByIdAndDelete(id) //replaces findindex and splice 
+
+    if(!deletedCat){ //findbyIdAndDelete returns null if it cannot find a Cat that matches the id
         return res.status(404).json({error: 'Cat not found'})
     }
-    cats.splice(index, 1)
     return res.status(204).end()
 })
 
-//events routes
-app.get('/events', (req, res) => {
+//events routes - expressJS
+// app.get('/events', (req, res) => {
+//     res.json(events)
+// })
+
+// app.post('/events', (req, res) => {
+//     const newEvent = req.body//req.body is where Express puts the data sent from the client (our app)
+
+//     if(!newEvent || !newEvent.title || !newEvent.date){
+//         //guarding against missing data in the request body
+//         return res.status(400).json({error:'Missing required event field(s)'})
+//         //status 400 means bad request
+//     }
+
+//     events.push(newEvent) //add new event to array by directly mutating it
+//     res.status(201).json(newEvent) //send back the new event back as a response to confirm what got saved, instead of res.send('ok')
+//     // status 200 means 'ok', status 201 means 'created' - response.ok is true for status codes 200-299
+// })
+
+// app.delete('/events/:id', (req, res) => {
+//     const {id} = req.params
+//     const index = events.findIndex(event => String(event.id) === id) //findIndex finds the array index where the items is in the array
+
+//     if (index === -1){ //find index returns -1 if the item is not found
+//         return res.status(404).json({error: 'event not found'}) // status 404 means 'not found'
+//     }
+//     events.splice(index, 1) //splice is removing exactly 1 element starting at index, mutating the array in place
+//     res.status(204).end() //status 204 is a successful response for no content //.end() sends the response with an empty body
+
+// })
+
+
+// event routes - mongoose
+app.get('/events', async(req, res) =>{
+    const events = await Event.find()
     res.json(events)
 })
 
-app.post('/events', (req, res) => {
-    const newEvent = req.body//req.body is where Express puts the data sent from the client (our app)
+app.post('/events', async(req, res) =>{
+    const newEvent = req.body
 
-    if(!newEvent || !newEvent.title || !newEvent.date){
-        //guarding against missing data in the request body
-        return res.status(400).json({error:'Missing required event field(s)'})
-        //status 400 means bad request
+    if (!newEvent || !newEvent.title || !newEvent.date) {
+        return res.status(400).json({Error: 'New event is missing required fields'})
     }
 
-    events.push(newEvent) //add new event to array by directly mutating it
-    res.status(201).json(newEvent) //send back the new event back as a response to confirm what got saved, instead of res.send('ok')
-    // status 200 means 'ok', status 201 means 'created' - response.ok is true for status codes 200-299
+    const createdEvent = await Event.create(newEvent)
+    res.status(201).json(createdEvent)
 })
 
-app.delete('/events/:id', (req, res) => {
+app.delete('/events/:id', async(req, res) =>{
     const {id} = req.params
-    const index = events.findIndex(event => String(event.id) === id) //findIndex finds the array index where the items is in the array
+    const deletedEvent = await Event.findByIdAndDelete(id)
 
-    if (index === -1){ //find index returns -1 if the item is not found
-        return res.status(404).json({error: 'event not found'}) // status 404 means 'not found'
+    if(!deletedEvent){
+        return res.status(404).json({error:'Event not found'})
     }
-    events.splice(index, 1) //splice is removing exactly 1 element starting at index, mutating the array in place
-    res.status(204).end() //status 204 is a successful response for no content //.end() sends the response with an empty body
 
+    res.status(204).end()
 })
 
 
