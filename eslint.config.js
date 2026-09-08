b281de565f5ad6cb0ac5ec73ec95e240629b3e19
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-09-07 19:00:30 -0700

    Configure global node language option

--- eslint.config.js
+++ eslint.config.js
@@ -21,4 +21,10 @@ export default defineConfig([
       'no-unused-vars': 'warn'
     },
   },
+  {
+    files: ['server/**/*.js'],
+    languageOptions: {
+      globals: globals.node
+    }
+  }
 ])
