ddac247a023ce03e9269152629f79b311a980ea0
flo-in-code <227476052+flo-in-code@users.noreply.github.com>
2026-09-09 16:49:33 -0700

    Add patch routes for cats and events

--- server/index.js
+++ server/index.js
@@ -71,6 +71,21 @@ app.delete('/cats/:id', async (req, res) =>{
     return res.status(204).end()
 })
 
+app.patch('/cats/:id', async (req, res) =>{
+    const {id} = req.params
+    const updates = req.body //passing a plain object. mongoose treats a plain object like this as a shorthand for $set, meaning only the fields you included gets changed, and every other field on the document gets untouched. This is what mongoose does to a PATCH(partial update) rather than PUT(full replace)
+    const updatedCat = await Cat.findByIdAndUpdate(id, updates, {returnDocument: 'after', runValidators: true})
+    //we need the settings object {returnDocument: 'after'} or {new: true} (older version) because it controls which snapshot of the document gets handed as the return value. MongoDB applies $set: {notes: "new notes"} and saves it. 
+    //without {returnDocument: 'after'}, MongoDB by default captures the snapshot of the document the instant BEFORE applying the changes and that is the default return value of findByIdAndUpdate(..), so even though mongoDB will hold the updated document, the value you're capturing by default with findByIdAndUpdate will be stale 
+    //with {returnDocument: 'after'}, mongoDB hands back a snapshot taken after the changes were committed to match what's in the database after the changes were applied.
+    //{runValidators: true} validates rules like required:true in the Schema. Mongoose does not run schema validation rules on updates, only on creation. So without {runValidators: true} we could patch a cat's name to an empty string and Mongoose would allow it to silently bypass the same rules that protects POST.
+
+    if(!updatedCat){
+        return res.status(404).json({error:'Cat not found'})
+    }
+
+    res.status(200).json(updatedCat)
+})
 
 
 // event routes - mongoose
@@ -99,6 +114,18 @@ app.delete('/events/:id', async(req, res) =>{
     }
 
     res.status(204).end()
+})
+
+app.patch('/events/:id', async(req, res) => {
+    const {id} = req.params
+    const updates = req.body
+    const updatedEvent = await Event.findByIdAndUpdate(id, updates, {returnDocument: 'after', runValidators: true})
+
+    if (!updatedEvent){
+        return res.status(404).json({error: 'Event not found'})
+    }
+
+    res.status(200).json(updatedEvent)
 })
 
 
